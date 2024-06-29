add5631e3ea1adc861be9df5e704accf6c3a0475
Akrit Rihal <55195493+AkritRihal@users.noreply.github.com>
2024-06-30 02:25:59 +0530

    Update EthAvaxAssessment1.sol

--- EthAvaxModule1/EthAvaxAssessment1.sol
+++ EthAvaxModule1/EthAvaxAssessment1.sol
@@ -1,30 +1,33 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
-contract ExampleContract {
-    uint256 public value;
-
-    constructor(uint256 _initialValue) {
-        require(_initialValue > 0, "Initial value must be greater than zero");
-        value = _initialValue;
+contract StudentData {
+    struct Student {
+        uint256 id;
+        string name;
+        uint8 age;
     }
 
-    function setValue(uint256 _value) public {
-        require(_value > 0, "Value must be greater than zero");
-        value = _value;
+    Student public student;
+
+    constructor(uint256 _id, string memory _name, uint8 _age) {
+        require(_id > 0, "Student ID must be greater than zero");
+        require(_age > 0, "Age must be greater than zero");
+        student = Student(_id, _name, _age);
     }
 
-    function incrementValue() public {
-        uint256 newValue = value + 1;
-        assert(newValue > value); 
-        value = newValue;
+    function updateStudent(uint256 _id, string memory _name, uint8 _age) public {
+        require(_id > 0, "Student ID must be greater than zero");
+        assert(_age > student.age); // Ensure newAge is greater than current age
+        student.age = _age;
+        student.name = _name;
+        student.id = _id;
     }
 
-    function resetValue() public {
-        if (value <= 10) {
-            revert("Current value is too low to reset");
+    function passStudent() public {
+        if (student.age <= 5) {
+            revert("Student age is too low for next class");
         }
-        value = 0;
-        
+        student = Student(0, "", 0);
     }
 }
